ce39dc0bffecae03eb228cb21e7c798d60ef9829
sam goetmaeker <sam.goetmaeker@gmail.com>
2014-04-02 15:20:53 +0200

    can't save an e-mail?????

--- db/migrate/20140402123847_add_email_uniqueness_index.rb
+++ db/migrate/20140402123847_add_email_uniqueness_index.rb
@@ -1,5 +1,8 @@
 class AddEmailUniquenessIndex < ActiveRecord::Migration
-  def change
-  	add_index :users, :email, :unique => true
+  def self.up
+  	add_index :users, :email, :unique : true
+  end
+  def self.down
+  	remove_index :users, :email
   end
 end
