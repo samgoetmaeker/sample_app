f24cb19a727a2f9ac54e113e35bf0fe498c8adc7
sam goetmaeker <sam.goetmaeker@gmail.com>
2014-04-02 14:42:22 +0200

    User model index op email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-	attr_accesible :name, :email
+	attr_accessor :name, :email
+
+	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :nickname, 	:presence => true,
+							:length => { :maximum => 50 }
+	validates :email, 		:presence => true,
+							:uniqueness => { :case_sensitive => false },
+							:format => { :with => email_regex }
+
 end
