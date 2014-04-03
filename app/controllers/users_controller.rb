4f9557109440c93b53f1fe2a43e49ea43e7bd7e9
sam goetmaeker <sam.goetmaeker@gmail.com>
2014-04-03 10:21:42 +0200

    added view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
-  def new
-  	@title = "Sign Up"
-  end
+  	def new
+  		@title = "Sign Up"
+  		@user = User.new
+  	end
 
-  def show
-	@user = User.find(params[:id])  
+  	def show
+		@user = User.find(params[:id])  
 
-  end
+ 	end
+
+	def create
+		@user = User.new(params[:id])
+
+		if @user.save
+			redirect_to root_url
+		else
+			render "new"
+		end
+	end
 
 end
